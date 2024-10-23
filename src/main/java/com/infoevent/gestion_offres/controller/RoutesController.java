d5b4954807c287349d841e4c71c91cc6c3899d0a
PalouSOUANGUIPALI <samparialzouma91@gmail.com>
2024-10-23 15:28:39 +0200

    changement de tous les fichiers

--- src/main/java/com/infoevent/gestion_offres/controller/RoutesController.java
+++ src/main/java/com/infoevent/gestion_offres/controller/RoutesController.java
@@ -1,5 +1,6 @@
 package com.infoevent.gestion_offres.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,20 @@ import java.util.List;
 @Controller
 public class RoutesController {
 
+    // Injecter la base URL de gestion-offres depuis les propriétés
+    @Value("${gestion.offres.base.url}")
+    private String baseUrl;
+
     @GetMapping("/routes")
     public String showRoutes(Model model) {
         // Liste des actions que l'administrateur peut effectuer
         List<String> routes = Arrays.asList(
-                "/users/home - Acceder au site",
-                "/users/connexion - Connexion pour gérer les offres",
-                "/users/sold-by-type - Voir les offres vendues par types d'offres"
+                baseUrl + "/users/home - Accéder au site",
+                baseUrl + "/users/connexion - Connexion pour gérer les offres",
+                baseUrl + "/users/sold-by-type - Voir les offres vendues par types d'offres"
         );
 
         model.addAttribute("routes", routes);
         return "index"; // Retourne le nom du template HTML index (index.html)
     }
 }
-
